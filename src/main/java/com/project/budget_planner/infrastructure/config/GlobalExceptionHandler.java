877519725c95fd4849b37405c33dc2124ed7e111
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-11-07 20:17:21 +0300

    feat(config): manages authentication exceptions

--- src/main/java/com/project/budget_planner/infrastructure/config/GlobalExceptionHandler.java
+++ src/main/java/com/project/budget_planner/infrastructure/config/GlobalExceptionHandler.java
@@ -1,13 +1,26 @@
 package com.project.budget_planner.infrastructure.config;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Gestion des exceptions génériques
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur est survenue : " + ex.getMessage());
+    }
 
     // Gestion des IllegalArgumentException
     @ExceptionHandler(IllegalArgumentException.class)
@@ -15,9 +28,33 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
-    // Gestion des autres exceptions génériques
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur est survenue : " + ex.getMessage());
+    // The username or password is incorrect
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatusCode.valueOf(401)).body(ex.getMessage());
+    }
+
+    // The account is locked
+    @ExceptionHandler(AccountStatusException.class)
+    public ResponseEntity<String> handleAccountStatusException(AccountStatusException ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(ex.getMessage());
+    }
+
+    // Not authorized to access the resource
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(ex.getMessage());
+    }
+
+    // The JWT signature is invalid
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<String> handleSignatureException(SignatureException ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(ex.getMessage());
+    }
+
+    // The JWT token has expired
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(ex.getMessage());
     }
 }
