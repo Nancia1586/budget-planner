1760529459a7f77e6b1e46e60d1af633fbff2051
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-11-07 20:18:36 +0300

    feat(domain.service): create service for signin and signup actions

--- src/main/java/com/project/budget_planner/domain/service/UserService.java
+++ src/main/java/com/project/budget_planner/domain/service/UserService.java
@@ -6,6 +6,9 @@ import java.util.UUID;
 
 import com.project.budget_planner.domain.model.User;
 import com.project.budget_planner.domain.repository.UserRepository;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,30 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public User creatUser(User user) {
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        return userRepository.save(user);
+    }
+
+    public User signin(String email, String password) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        password
+                )
+        );
+        return userRepository.findByEmail(email)
+            .filter(user -> passwordEncoder.matches(password, user.getPassword()))
+            .orElseThrow(() -> new IllegalArgumentException("Email or password is incorrect"));
     }
 
     public void updatePassword(UUID userId, String newPassword) {
@@ -36,12 +59,6 @@ public class UserService {
 
     public Optional<User> getUserByEmail(String email) {
         return userRepository.findByEmail(email);
-    }
-
-    public User createUser(User user) {
-        String hashedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(hashedPassword);
-        return userRepository.save(user);
     }
 
     public void deleteUser(UUID id) {
