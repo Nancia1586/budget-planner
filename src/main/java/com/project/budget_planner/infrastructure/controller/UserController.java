4313a0d4efec5b9a423e65909f58928274fa2ff5
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-11-10 20:43:55 +0300

    feat(users): add getCurrentUser endpoint

--- src/main/java/com/project/budget_planner/infrastructure/controller/UserController.java
+++ src/main/java/com/project/budget_planner/infrastructure/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,9 +39,16 @@ public class UserController {
         return userApplicationService.getAllUsers();
     }
 
-    @GetMapping("/{username}")
-    public Optional<UserDto> getUserByUsername(@PathVariable String username) {
-        return userApplicationService.getUserByUsername(username);
+    @GetMapping("/{pseudo}")
+    public Optional<UserDto> getUserByPseudo(@PathVariable String pseudo) {
+        return userApplicationService.getUserByPseudo(pseudo);
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
     }
 
     @PostMapping("/signup")
